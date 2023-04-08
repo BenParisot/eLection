381b2a0098b6cfb022161b7ec8d2d8a312693cd1
Ben <me@benparisot.com>
2023-04-08 12:20:15 -0700

    adds party radio buttons to candidate adder

--- src/election_setup/CandidateAdder.jsx
+++ src/election_setup/CandidateAdder.jsx
@@ -5,10 +5,19 @@ const CandidateAdder = () => {
             <h1>Add candidates</h1>
             <form>
                 <label>candidate name</label>
-                <input type="name"></input>
+                <input type="radio"></input>
+                <p>candidate party</p>
+                <input type="radio" id="democrat" name="candidate-party" value="democrat"></input>
+                <label for="democrat">Democrat</label>
+                <input type="radio" id="republican" name="candidate-party" value="republican"></input>
+                <label for="republican">Republican</label>
+                <input type="radio" id="independent" name="candidate-party" value="independent"></input>
+                <label for="independent">Independent</label>
+                <input type="radio" id="other" name="candidate-party" value="other"></input>
+                <label for="other">Other</label>
                 <label>candidate name</label>
                 <input type="name"></input>
-                <button type="submit">Continue</button >
+                <button type="submit">Next</button >
             </form>
         </>
     )
